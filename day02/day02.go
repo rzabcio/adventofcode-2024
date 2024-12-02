fef85aa3db27f21950a67b2f0cdf19f34a129f71
Jakub Głazik <rzabcio@gmail.com>
2024-12-02 19:41:58 +0100

    feat(day2.2): wip: need to actually remove wrong report, not just count failures

--- day02/day02.go
+++ day02/day02.go
@@ -1,6 +1,8 @@
 package day02
 
 import (
+	"fmt"
+
 	"github.com/rzabcio/adventofcode-2024/utils"
 )
 
@@ -31,5 +33,48 @@ func Day02_1(filename string) (result int) {
 }
 
 func Day02_2(filename string) (result int) {
+	for row := range utils.InputRowsAsInts(filename) {
+		result++
+		firstWrongReport := false
+		firstDiff := row[1] - row[0]
+		fmt.Printf("%v\n", row)
+		for i := 0; i < len(row)-1; i++ {
+			diff := row[i+1] - row[i]
+			fmt.Printf("   %d - %d = %d\n", row[i+1], row[i], diff)
+			// if two numbers are the same
+			if diff == 0 {
+				if !firstWrongReport {
+					fmt.Println("   0: first")
+					firstWrongReport = true
+					continue
+				}
+				fmt.Println("   0: second")
+				result--
+				break
+			}
+			// if it is decreasing series and diff greates than zero or less than -3
+			if firstDiff < 0 && (diff > 0 || diff < -3) {
+				if !firstWrongReport {
+					fmt.Println("   1: first")
+					firstWrongReport = true
+					continue
+				}
+				fmt.Println("   1: second")
+				result--
+				break
+			}
+			// if it is increasing series and diff less than zero or greater than 3
+			if firstDiff > 0 && (diff < 0 || diff > 3) {
+				if !firstWrongReport {
+					fmt.Println("   2: first")
+					firstWrongReport = true
+					continue
+				}
+				fmt.Println("   2: second")
+				result--
+				break
+			}
+		}
+	}
 	return result
 }
