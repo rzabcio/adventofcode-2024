c6d5e575050ac0edadc7644229c01ee724744ea8
Jakub Głazik <rzabcio@gmail.com>
2024-12-07 22:05:19 +0100

    feat(day3.2): pretty nice usage of the code from part 1 - additional regex rules + some ifs

--- day03/day03.go
+++ day03/day03.go
@@ -9,20 +9,34 @@ import (
 )
 
 func Day03_1(filename string) (result int) {
+	return multiply(filename)
+}
+
+func Day03_2(filename string) (result int) {
+	return multiply(filename)
+}
+
+func multiply(filename string) (result int) {
+	enabled := true
 	for line := range utils.InputRows(filename) {
-		r1 := regexp.MustCompile(`mul\(\d+,\d+\)`)
-		for _, mul := range r1.FindAllString(line, -1) {
-			mul = strings.ReplaceAll(mul, "mul(", "")
-			mul = strings.ReplaceAll(mul, ")", "")
-			numbers := strings.Split(mul, ",")
+		r1 := regexp.MustCompile(`(mul\(\d+,\d+\)|do\(\)|don't\(\))`)
+		for _, part := range r1.FindAllString(line, -1) {
+			if part == "don't()" {
+				enabled = false
+				continue
+			} else if part == "do()" {
+				enabled = true
+				continue
+			} else if !enabled {
+				continue
+			}
+			part = strings.ReplaceAll(part, "mul(", "")
+			part = strings.ReplaceAll(part, ")", "")
+			numbers := strings.Split(part, ",")
 			n1, _ := strconv.Atoi(numbers[0])
 			n2, _ := strconv.Atoi(numbers[1])
 			result += n1 * n2
 		}
 	}
-	return result
-}
-
-func Day03_2(filename string) (result int) {
 	return result
 }
--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/rzabcio/adventofcode-2024/day01"
 	"github.com/rzabcio/adventofcode-2024/day02"
+	"github.com/rzabcio/adventofcode-2024/day03"
 )
 
 func TestDay01(t *testing.T) {
@@ -17,12 +18,21 @@ func TestDay01(t *testing.T) {
 		t.Errorf("Day01_2(test1) = %d; want %d", got, want)
 	}
 
-	got, want = day02.Day02_1("input-files/day02-test1"), 2
+	got, want = day02.Day02_1("input-files/day02-test"), 2
 	if got != want {
 		t.Errorf("Day02_1(test1) = %d; want %d", got, want)
 	}
-	got, want = day02.Day02_2("input-files/day02-test1"), 0
+	got, want = day02.Day02_2("input-files/day02-test"), 6
 	if got != want {
 		t.Errorf("Day02_2(test1) = %d; want %d", got, want)
+	}
+
+	got, want = day03.Day03_1("input-files/day03-test"), 161
+	if got != want {
+		t.Errorf("Day03_1(test) = %d; want %d", got, want)
+	}
+	got, want = day03.Day03_2("input-files/day03-test2"), 48
+	if got != want {
+		t.Errorf("Day03_2(test1) = %d; want %d", got, want)
 	}
 }
