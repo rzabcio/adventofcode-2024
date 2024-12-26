548201bffbbaa19a4671ad26a059e2804e99ca1b
Jakub Glazik <rzabcio@gmail.com>
2024-12-26 13:47:01 +0100

    feat(day8.1): pretty overengineer, but the solution itself is pretty good imo
    built two objects Antenna and AntennaMap, the latter with some functions
    to ease switching in part 2
    map of the whole antenna map is build, but not used in the actual
    antinodes searching/creating - we use there map type (rune) to antennas
    list structure - that's where the most optimization is I think

--- day08/day08.go
+++ day08/day08.go
@@ -8,7 +8,7 @@ import (
 
 func Day08_1(filename string) (result int) {
 	am := NewAntennaMap(filename)
-	fmt.Println(am)
+	result = am.FindAllAntinodes()
 	return result
 }
 
@@ -17,22 +17,25 @@ func Day08_2(filename string) (result int) {
 }
 
 type AntennaMap struct {
-	area         map[int]map[int]Antenna
+	area         map[int]map[int]*Antenna
+	antennas     map[rune][]*Antenna
 	sizeX, sizeY int
 }
 
 func NewAntennaMap(filename string) (am AntennaMap) {
 	am = AntennaMap{}
-	am.area = make(map[int]map[int]Antenna)
+	am.area = make(map[int]map[int]*Antenna)
+	am.antennas = make(map[rune][]*Antenna)
 	y := 0
 	for line := range utils.InputRows(filename) {
 		am.sizeX = len(line)
 		for x, r := range line {
 			if _, ok := am.area[x]; !ok {
-				am.area[x] = make(map[int]Antenna)
+				am.area[x] = make(map[int]*Antenna)
 			}
 			a := NewAntenna(r, x, y)
 			am.area[x][y] = a
+			am.antennas[r] = append(am.antennas[r], a)
 		}
 		y++
 	}
@@ -44,9 +47,9 @@ func (am AntennaMap) String() (s string) {
 	for y := 0; y < am.sizeY; y++ {
 		for x := 0; x < am.sizeX; x++ {
 			if a, ok := am.area[x][y]; ok {
-				s += string(a.antenna)
+				s += a.String()
 			} else {
-				s += "."
+				s += ". "
 			}
 		}
 		s += "\n"
@@ -54,20 +57,89 @@ func (am AntennaMap) String() (s string) {
 	return s
 }
 
+func (am *AntennaMap) FindAllAntinodes() int {
+	for r := range am.antennas {
+		if r == '.' {
+			continue
+		}
+		am.FindAntinodes(r)
+	}
+	return am.CountAntinodes()
+}
+
+func (am *AntennaMap) FindAntinodes(r rune) {
+	for i := 0; i < len(am.antennas[r])-1; i++ {
+		a1 := am.antennas[r][i]
+		for j := i + 1; j < len(am.antennas[r]); j++ {
+			a2 := am.antennas[r][j]
+			am.FindAntinodesForAntennas(a1, a2)
+		}
+	}
+}
+
+func (am *AntennaMap) FindAntinodesForAntennas(a1, a2 *Antenna) {
+	distX := a1.x - a2.x
+	distY := a1.y - a2.y
+	// antinode 1
+	antiX, antiY := a1.x+distX, a1.y+distY
+	am.MarkAntinode(antiX, antiY, a1.antenna)
+	// antinode 2
+	antiX, antiY = a2.x-distX, a2.y-distY
+	am.MarkAntinode(antiX, antiY, a2.antenna)
+}
+
+func (am *AntennaMap) MarkAntinode(x, y int, r rune) bool {
+	var a *Antenna
+	var ok bool
+	if am.InBounds(x, y) {
+		if a, ok = am.area[x][y]; !ok {
+			a = NewAntenna('.', x, y)
+		}
+		a.antinodes = append(a.antinodes, r)
+		am.area[x][y] = a
+		return true
+	}
+	return false
+}
+
+func (am *AntennaMap) InBounds(x, y int) bool {
+	return x >= 0 && x < am.sizeX && y >= 0 && y < am.sizeY
+}
+
+func (am *AntennaMap) CountAntinodes() (count int) {
+	for y := 0; y < am.sizeY; y++ {
+		for x := 0; x < am.sizeX; x++ {
+			if a, ok := am.area[x][y]; ok {
+				if a.IsAntinode() {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
 type Antenna struct {
 	x, y      int
 	antenna   rune
-	antinodes []string
+	antinodes []rune
 }
 
-func NewAntenna(r rune, x, y int) (a Antenna) {
-	a = Antenna{}
+func NewAntenna(r rune, x, y int) (a *Antenna) {
+	a = &Antenna{}
 	a.x = x
 	a.y = y
 	a.antenna = r
 	return a
 }
 
+func (a *Antenna) IsAntinode() bool {
+	return len(a.antinodes) > 0
+}
+
 func (a Antenna) String() (s string) {
-	return fmt.Sprintf("[%d,%d] %c (%v)", a.x, a.y, a.antenna, a.antinodes)
+	if a.IsAntinode() {
+		return fmt.Sprintf("%c#", a.antenna)
+	}
+	return fmt.Sprintf("%c ", a.antenna)
 }
